b27c7cae5efa343776ac72336dc248beec5292e4
Barrett Kepler <45532267+Kepler-Br@users.noreply.github.com>
2019-06-29 15:44:55 +0300

    Transform to camera space. Still no renderer

--- renderengine.h
+++ renderengine.h
@@ -27,112 +27,10 @@ class RenderEngine
         return glm::vec2(x, y);
     }
 
-    void renderMap()
-    {
-        for(auto &sector : sectors)
-        {
-            const glm::vec3 bias = glm::vec3(200.0f, 300.0f, 100.0f);
-            window.setColor(glm::vec3(1.0f));
-            window.square(player.position+bias-glm::vec3(3.0f), glm::vec2(6.0f));
-            constexpr float lookLength = 20.0f;
-            glm::vec2 playerSight = glm::normalize(player.position-glm::vec3(player.position.x + cos(player.angle)*lookLength,
-                                                                             player.position.y + sin(player.angle)*lookLength, 0.0f));
-            window.line(player.position+bias, glm::vec3(player.position.x + cos(player.angle)*lookLength, player.position.y + sin(player.angle)*lookLength, 0.0f)+bias);
+    void renderMap();
 
-            const int &startWallIndex = sector.startWall;
-            const int &totalWalls = sector.numWalls;
-            const Wall startWall = walls[startWallIndex];
-            Wall currentWall = startWall;
-            for(int wallIndex = startWallIndex; wallIndex < startWallIndex+totalWalls; wallIndex++)
-            {
-                const glm::vec2 &curPoint = currentWall.point + glm::vec2(bias);
-                const int nextPointIndex = currentWall.nextWallIndex;
-                const glm::vec2 &nextPoint = walls[nextPointIndex].point + glm::vec2(bias);
-                if(currentWall.nextSectorIndex == -1)
-                    window.setColor(glm::vec3(1.0f));
-                else
-                    window.setColor(glm::vec3(1.0f, 0.0f, 0.0f));
-                window.line(curPoint, nextPoint);
-
-                currentWall = walls[currentWall.nextWallIndex];
-            }
-        }
-    }
-
-    void render3D()
-    {
-        glm::vec2 bias = glm::vec2(400.0f, 300.0f);
-        Sector currentSector = sectors[player.lastSector];
-        Wall currentWall = walls[currentSector.startWall];
-        for(int i = 0; i < currentSector.numWalls; i++)
-        {
-            Wall nextWall = walls[currentWall.nextWallIndex];
-
-            glm::vec2 translateOne = currentWall.point - glm::vec2(player.position);
-            glm::vec2 translateTwo = nextWall.point - glm::vec2(player.position);
-
-            glm::vec3 rotateOne = glm::vec3(translateOne.x*player.angleSin-translateOne.y*player.angleCos,
-                                            0.0f,
-                                            translateOne.x*player.angleCos+translateOne.y*player.angleSin);
-            glm::vec3 rotateTwo = glm::vec3(translateTwo.x*player.angleSin-translateTwo.y*player.angleCos,
-                                            0.0f,
-                                            translateTwo.x*player.angleCos+translateTwo.y*player.angleSin);
-
-            // If wall behind player.
-            if(rotateOne.z <= 0.0f && rotateTwo.z  <= 0.0f)
-                continue;
-
-            // Clip if partially behind.
-            if(rotateOne.z <= 0.0f || rotateTwo.z  <= 0.0f)
-            {
-                float nearz = 1e-4f;
-                float farz = 5.0f;
-                float nearside = 1e-5f;
-                float farside = 20.0f;
-
-                glm::vec2 i1 = lineIntersection(rotateOne, rotateTwo, glm::vec2(-nearside, nearz), glm::vec2(-farside, farz));
-                glm::vec2 i2 = lineIntersection(rotateOne, rotateTwo, glm::vec2(nearside, nearz), glm::vec2(farside, farz));
-                if(rotateOne.z < nearz)
-                {
-                    if(i1.y > 0)
-                    {
-                        rotateOne.x = i1.x;
-                        rotateOne.z = i1.y;
-                    }
-                    else
-                    {
-                        rotateOne.x = i2.x;
-                        rotateOne.x = i2.y;
-                    }
-                }
-
-                if(rotateTwo.z < nearz)
-                {
-                    if(i1.y > 0)
-                    {
-                        rotateTwo.x = i1.x;
-                        rotateTwo.z = i1.y;
-                    }
-                    else
-                    {
-                        rotateTwo.x = i2.x;
-                        rotateTwo.x = i2.y;
-                    }
-                }
-            }
-
-            constexpr float aspectRatio = 800.0f/600.0f;
-            window.line(glm::vec2(rotateOne.x, currentSector.floor/rotateOne.z)*aspectRatio + bias,
-                        glm::vec2(rotateTwo.x, currentSector.floor/rotateTwo.z)*aspectRatio + bias);
-            window.line(glm::vec2(rotateOne.x, currentSector.ceiling/rotateOne.z)*aspectRatio + bias,
-                        glm::vec2(rotateTwo.x, currentSector.ceiling/rotateTwo.z)*aspectRatio + bias);
-
-//            window.line(glm::vec2(rotateOne.x, currentSector.ceiling/rotateOne.z) + bias,
-//                        glm::vec2(rotateTwo.x, currentSector.ceiling/rotateTwo.z) + bias);
-
-            currentWall = nextWall;
-        }
-    }
+    void render3D();
+    void renderRotatedMap();
 
     float lerp(float v0, float v1, float t)
     {
@@ -145,7 +43,7 @@ class RenderEngine
         {
             return one.y+(x-one.x)*(two.y-one.y)/(two.x-one.x);
         };
-        for(int i = pointOne.x; i < pointTwo.x; i++)
+        for(int i = glm::min(pointOne.x, pointTwo.x); i < glm::max(pointOne.x, pointTwo.x); i++)
         {
             float y = interpolation(pointOne, pointTwo, i);
             window.vLine(glm::vec2(i, y), lerp(heightOne, heightTwo, i/pointTwo.x));
@@ -166,6 +64,7 @@ public:
     void render()
     {
         renderMap();
+        renderRotatedMap();
         render3D();
 //        drawFilledSquare(glm::vec2(0.0f, 100.0f), glm::vec2(100.0f, 100.0f), 10, 20);
     }
